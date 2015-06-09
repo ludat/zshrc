ac5cab9e17ef9e51b10d8f56dc920088cb21652b
Lucas David Traverso <lucas6246@gmail.com>
2015-06-09 04:04:58 -0300

    Added more sensible history options and descriptions

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -14,11 +14,25 @@ case $HIST_STAMPS in
   *) alias history='fc -l 1' ;;
 esac
 
-setopt append_history
-setopt extended_history
-setopt hist_expire_dups_first
-setopt hist_ignore_dups # ignore duplication command history list
-setopt hist_ignore_space
-setopt hist_verify
-setopt inc_append_history
-setopt share_history # share command history data
+# Append history to the histfile instead of making a new one useless when INC_APPEND_HISTORY is set
+# setopt APPEND_HISTORY
+# Add the timestamp and the duration of each command to HISTFILE
+# setopt EXTENDED_HISTORY
+# When HISTSIZE is reached remove repeated commands before
+setopt HIST_EXPIRE_DUPS_FIRST
+# Never save duplicated lines to the history
+setopt HIST_IGNORE_ALL_DUPS
+# Don't save to HISTFILE commands starting with a space
+setopt HIST_IGNORE_SPACE
+# Reduce spaces that mean nothing to the shell
+setopt HIST_REDUCE_BLANKS
+# Never save duplicated commands to the HISTFILE
+setopt HIST_SAVE_NO_DUPS
+# Expand hist instead of executing command
+setopt HIST_VERIFY
+# Add command to HISTFILE as soon as they are executed
+#   This also makes adding the timestamp imposible since the addition takes place before the command is executed
+setopt INC_APPEND_HISTORY
+# Autoload history from HISTFILE and set INC_APPEND_HISTORY and EXTENDED_HISTORY
+# to force syncing execute `fc -Rl`
+# setopt SHARE_HISTORY
